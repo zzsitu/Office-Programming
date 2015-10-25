3ae05c72a94ff74c12f638315615f118fcaf67c4
zzsitu <zzsitu@zzmac.local>
2015-10-26 00:30:53 +0800

    add input parameter

--- deletePdfPage.py
+++ deletePdfPage.py
@@ -1,17 +1,37 @@
-# Merge two PDFs
+# install:
+# pip install PyPDF2
+
+import sys
 from PyPDF2 import PdfFileReader, PdfFileWriter
 
-def deletePage(inputPath, outputPath,index):
+def deletePage(inputPath,outputPath,index):
     output = PdfFileWriter()
     pdf = PdfFileReader(file(inputPath , "rb"))
 
     numOfPages = pdf.getNumPages()
     for i in range(0, numOfPages):
         if i != index:
+            print i,index
             output.addPage(pdf.getPage(i))
 
     outputStream = file(outputPath, "wb")
     output.write(outputStream)
     outputStream.close()
 
-deletePage("output.pdf","tmp.pdf",0)
+def helpMsg():
+    print "\n\
+    usage: deletePdfPage.py [inputFile] [outputFile] [pageToDelete]\n\
+    "
+
+def main():
+    if len(sys.argv)!=3:
+        helpMsg()
+        return
+
+    inputPath = sys.argv[1]
+    outputPath = sys.argv[2]
+    index = int(sys.argv[3])
+    deletePage(inputPath, outputPath,index)
+
+if __name__ == '__main__':
+    main()
